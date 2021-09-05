b818cf8540f6015c1d075a56ffaebbaea5e640f2
asd40441 <asd40441@hotmail.com>
2021-09-05 23:08:29 +0800

    Update 2021-09-05T15:08:27.421Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,9 @@
 "use strict";
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
 //# sourceMappingURL=all.js.map
